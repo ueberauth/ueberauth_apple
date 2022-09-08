09e67b0bb274c6276d599b44d84ad09ff047d6a2
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-09-08 11:10:04 -0400

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -28,10 +28,10 @@ defmodule UeberauthApple.Mixfile do
   defp deps do
     [
       {:oauth2, "~> 2.0"},
-      {:ueberauth, "~> 0.5"},
+      {:ueberauth, "~> 0.10"},
       {:jose, "~> 1.0"},
       {:httpoison, "~> 1.0"},
-      {:credo, "~> 0.8", only: [:dev, :test]},
+      {:credo, "~> 1.6", only: [:dev, :test]},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.3", only: :dev}
     ]
